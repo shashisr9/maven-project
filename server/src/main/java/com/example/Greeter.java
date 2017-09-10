72cf7bd70ddac344b5ab0c04f61b3cfc4c250fc0
shashisr9 <srshashidhar7@gmail.com>
2017-09-10 20:36:27 +0530

    Update Greeter.java
    Update method param with final and added javadoc comments

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+  * @param: string - accepts greeting message
+  * @return string - greeting message
+  */
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
