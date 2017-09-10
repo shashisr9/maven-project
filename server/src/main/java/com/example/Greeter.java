f725711cb0f150cfae323d52ce2558abb1b669be
shashisr9 <srshashidhar7@gmail.com>
2017-09-10 21:20:43 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
   * @param someone - accepts greeting message
   * @return string - greeting message
   */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
